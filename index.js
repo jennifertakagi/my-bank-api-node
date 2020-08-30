9e82192e32a9df42f2e8cb48eec400e81ee3fe72
jennifertakagi <jennitakagi@gmail.com>
2020-08-30 10:24:41 -0300

    feat: add winston as logger service

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 import express from 'express';
+import winston from 'winston';
+import cors from 'cors';
+
 import accountRouter from './routes/accountRouter.js';
 
 const app = express();
+const { combine, timestamp, label, printf } = winston.format;
+const formatter = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+  level: 'silly',
+  format: combine(
+      label({ label: 'grade-control.log'}),
+      timestamp(),
+      formatter
+  ),
+  transports: [
+      new (winston.transports.Console)(),
+      new (winston.transports.File)({ filename: 'grades-control.log' }),
+  ]
+});
 
 app.use(express.json());
+app.use(cors());
 app.use('/accounts', accountRouter);
 
 app.listen(3031, async () => {
-    // try {
-    //     await fs.readFile(fileName);
-
-    //     logger.info('API started');
-    // } catch(error) {
-    //     const initialJSON = {
-    //         nextId: 1,
-    //         grades: []
-    //     };
-    //     fs.writeFile(fileName, JSON.stringify(initialJSON))
-    //         .then(() => {
-    //             logger.info('API started and file created!');
-    //         })
-    //         .catch((error) => {
-    //             logger.error(error);
-    //         });
-    // }
+    try {
+      logger.info('API started');
+    } catch(error) {
+      logger.error(error);
+    }
 });
