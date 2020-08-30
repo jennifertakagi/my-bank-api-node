4b04574d3e2ecd1377130072b027d9fab2c1efcb
jennifertakagi <jennitakagi@gmail.com>
2020-08-30 09:39:48 -0300

    feat: add mongoose schema

---
+++ models/accountModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const accountSchema = mongoose.Schema({
+  agencia: {
+    type: Number,
+    require: true,
+    min: 0
+  },
+  conta: {
+    type: Number,
+    require: true,
+    min: 0
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  balance: {
+    type: Number,
+    require: true
+  }
+});
+
+const accountModel = moongose.model('accounts', accountSchema, 'accounts');
+
+export {accountModel};
