ba7182b1d58a529d18cf27a931b92527793490f7
jennifertakagi <jennitakagi@gmail.com>
2020-08-30 15:36:20 -0300

    refactor: split function to validate params, validate account and format as currency

--- controller/AccountController.js
+++ controller/AccountController.js
@@ -12,16 +12,9 @@ class AccountController {
     try {
       let { agency, account, balance } = req.body;
 
-      if (!account || !agency ||  !balance) {
-        throw new Error('These parameters are required: account, agency, balance!');
-      }
+      validateRequestParams({account, agency, balance});
 
-      const accountDB = await accountModel.findOne({'account': account})
-
-      if (!accountDB) {
-        throw new Error(`The account ${account} doesn't exist!`);
-      }
-
+      const accountDB = await validateAccountExists(accountModel, account);
       const query = { _id: accountDB._id} ;
       const update = {
         "$inc": {
@@ -51,16 +44,9 @@ class AccountController {
     try {
       let { agency, account, balance } = req.body;
 
-      if (!account || !agency ||  !balance) {
-        throw new Error('These parameters are required: account, agency, balance!');
-      }
+      validateRequestParams({account, agency, balance});
 
-      const accountDB = await accountModel.findOne({'account': account})
-
-      if (!accountDB) {
-        throw new Error(`The account ${account} doesn't exist!`);
-      }
-
+      const accountDB = await validateAccountExists(accountModel, account);
       const taxWithdrawal = 1;
       const newBalance = accountDB.balance - taxWithdrawal - balance;
 
@@ -85,6 +71,67 @@ class AccountController {
         next(error);
     }
   }
+
+  /**
+   * Makes withdrawal according to account, and
+   * update the account's balance
+   * @param {Object} req request object
+   * @param {Object} res response object
+   * @param {Function} next next function
+  */
+  async getBalance(req, res, next) {
+    try {
+      let { agency, account } = req.body;
+
+      validateRequestParams({account, agency});
+
+      const accountDB = await validateAccountExists(accountModel, account);
+
+      res.send(`The balance is: ${formattedCurrency(accountDB.balance)}`);
+
+      logger.info(`GET /account/balance - ${JSON.stringify(accountDB.balance)}`);
+    } catch (error) {
+        next(error);
+    }
+  }
+}
+
+/**
+ * Validates if the request has all the required params 
+ * @param  {Array} params all params 
+ */
+function validateRequestParams(...params) {
+  const requestParams = (params && params.length && params[0]) || [];
+  const missingParams = Object.values(requestParams).some(value => !value)
+  if (missingParams) {
+    const requiredParams = Object.keys(requestParams).join(', ')
+    throw new Error(`These parameters are required: ${requiredParams}!`);
+  }
+}
+
+/**
+ * Validates if account exists on Database
+ * @param {Object} accountModel account model schema
+ * @param {String} account account identifier
+ * @returns {Object} index of user, if it does not exits returns -1
+ */
+async function validateAccountExists(accountModel, account = '') {
+  const accountDB =  await accountModel.findOne({'account': account});
+
+  if (!accountDB) {
+    throw new Error(`The account ${account} doesn't exist!`);
+  }
+
+  return accountDB;
+}
+
+/**
+ * Formats value to currency type
+ * @param {Number} value to be formatted
+ * @returns {String} formatted as currency
+ */
+function formattedCurrency(value) {
+	return value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
 
 export default new AccountController;
